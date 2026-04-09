f328ba820995320773daa074a780b58696311184
ics-ikeda <ikeda@ics-web.jp>
2026-04-09 13:58:13 +0900

    構成とアセット配置を整理してMain周辺を簡素化する

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,4 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   // Base public path when served in production
   base: './',
-
-  // Configure build output
-  build: {
-    // Output directory
-    outDir: 'docs',
-  },
 });
